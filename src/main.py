159b9a6c4ecbb666f040661acda1a6be2b61e21e
CherMet90 <dddfrdd@gmail.com>
2023-04-27 10:06:07 +0300

    SSL error handling

--- src/main.py
+++ src/main.py
@@ -2,15 +2,17 @@ import datetime
 import os
 import random
 import re
+import traceback
 
 import inflect
 import requests
 from colorama import Back, Fore, Style, init
+from requests.exceptions import SSLError
 
 
 class Word:
     OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-    
+
     def __init__(self, word, next_review_date):
         self.word = word
         self.next_review_date = next_review_date
@@ -26,27 +28,38 @@ class Word:
             lines = f.readlines()
             f.seek(0)
             for line in lines:
-                if line.startswith(self.word):
-                    f.write(f"{self.word},{self.next_review_date}\n")
-                else:
-                    f.write(line)
+                f.write(f"{self.word},{self.next_review_date}\n") if line.startswith(
+                    self.word) else f.write(line)
             f.truncate()
 
-    def get_gpt_definition(self): # Use the OpenAI API to get the definition
-        response = requests.post(
-            "https://api.openai.com/v1/completions",
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {self.OPENAI_API_KEY}"
-            },
-            json={
-                "prompt": f"Imagine you are dictionary for B1 english learners. Give an example sentence for '{self.word}' and provide some common synonyms.",
-                "model": 'text-davinci-002',
-                "max_tokens": 500,
-                "temperature": 0.35,
-                "n": 1
-            }
-        )
+    def get_gpt_definition(self):  # Use the OpenAI API to get the definition
+        retry = True
+        while retry:
+            try:
+                response = requests.post(
+                    "https://api.openai.com/v1/completions",
+                    headers={
+                        "Content-Type": "application/json",
+                        "Authorization": f"Bearer {self.OPENAI_API_KEY}"
+                    },
+                    json={
+                        "prompt": f"Imagine you are dictionary for B1 english learners. Give an example sentence for '{self.word}' and provide some common synonyms.",
+                        "model": 'text-davinci-002',
+                        "max_tokens": 500,
+                        "temperature": 0.35,
+                        "n": 1
+                    }
+                )
+                retry = False
+            except SSLError as e:
+                print(f"An SSL error occurred: {e}")
+                retry = input("Retry request? (y/n): ").lower() == 'y'
+                return
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print(traceback.format_exc())
+                return
+
         with open('gpt_response.txt', 'w', encoding='utf-8') as f:
             f.write(response.text)
         response_data = response.json()
@@ -77,10 +90,25 @@ class Word:
         print(self.origin)
 
 # Calculate the next review date for a word
+
+
 def calculate_next_review_date(word_instance, num_hidden_letters):
     days_to_wait = num_hidden_letters
     word_instance.next_review_date = (datetime.datetime.now(
     ) + datetime.timedelta(days=days_to_wait)).strftime('%Y-%m-%d')
+
+# Calculate the hidden word
+
+
+def hide_word(word, num_hidden_letters):
+    if num_hidden_letters == len(word):
+        num_hidden_letters -= 1
+        hidden_word = word[0] + '*' * num_hidden_letters
+    elif num_hidden_letters > 0:
+        num_hidden_letters -= 1
+        hidden_word = word[0] + '*' * num_hidden_letters + \
+            word[-(len(word)-num_hidden_letters)+1:]
+    return hidden_word, num_hidden_letters
 
 
 def play_game():
@@ -97,15 +125,20 @@ def play_game():
             break
         word = word.strip()
         next_review_date = None
-        if len(word.split(',')) == 2:
+        try:
             next_review_date = word.split(',')[1]
             print('(debug) ', next_review_date)
             next_review_date = datetime.datetime.strptime(
                 next_review_date, '%Y-%m-%d').date()
             if next_review_date > datetime.date.today():
                 continue
+        except IndexError:
+            pass
+
         word = word.split(',')[0]
         word_instance = Word(word, next_review_date)
+        if word_instance.definition is None:
+            continue
 
         correct_guess = False
         num_hidden_letters = len(word)
@@ -116,13 +149,8 @@ def play_game():
         correct_guess = guessed_word.lower() == word.lower()
 
         while not correct_guess:
-            if num_hidden_letters == len(word):
-                num_hidden_letters -= 1
-                hidden_word = word[0] + '*' * num_hidden_letters
-            elif num_hidden_letters > 0:
-                num_hidden_letters -= 1
-                hidden_word = word[0] + '*' * num_hidden_letters + \
-                    word[-(len(word)-num_hidden_letters)+1:]
+            hidden_word, num_hidden_letters = hide_word(
+                word, num_hidden_letters)  # recalculate the hidden word
             print(
                 f"{Back.RED}{Fore.WHITE}Sorry, the correct word is '{hidden_word}'.{Style.RESET_ALL}")
             print("Let's try again!")
@@ -134,11 +162,11 @@ def play_game():
         print(
             f"You saved {Fore.YELLOW}{num_hidden_letters}{Style.RESET_ALL} asterisk(s) and earned {Fore.YELLOW}{score}{Style.RESET_ALL} points.")
         total_score += score
-        
+
         if num_hidden_letters == 0:
             # All asterisks have been removed, so ask for the word's origin
             word_instance.make_one_more_request()
-        
+
         calculate_next_review_date(word_instance, num_hidden_letters)
         word_instance.update_review_date()
         print(
